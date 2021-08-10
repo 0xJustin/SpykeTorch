5575138d7ba385f3acd46e9840d4feca75c724cc
joycejm1 <justin.joyce@jhuapl.edu>
2021-08-10 17:32:25 -0400

    added setup.py file for pip install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spyketorch",
+    version="0.0.0",
+    description="justin setup for package for spyketorch",
+    url="https://github.com/miladmozafari/SpykeTorch",
+    packages=["spyketorch"],
+    zip_safe=False,
+)
+
